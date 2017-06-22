8d71a60f52dcef07e4228554e9f91773218505f9
Gur Chemel <g.chemel@ef.technion.ac.il>
2017-06-22 10:35:58 +0300

    Started working..

--- EX5/Testing/main.cpp
+++ EX5/Testing/main.cpp
@@ -1,6 +1,6 @@
-#ifndef _MY_SHARED_PTR_H_
-#define _MY_SHARED_PTR_H_
+#include<iostream>
 
+using namespace std;
 
 template<class T>
 class MySharedPtr {
@@ -9,14 +9,14 @@ public:
 	MySharedPtr(T* ptr) : pData(ptr), Counter_(1) {};
 	// Copy Constructor:
 	MySharedPtr(MySharedPtr<T>& from) : pData(from.pData), Counter_(++from.Counter_) {};
-	~MySharedPtr() {
+	~MySharedPtr() { 
 		if ((--Counter_) == 0) {
 			delete pData;
 		};
 	};
 	//class get() {return Ptr};
-	MySharedPtr& operator*() { return *pData; };
-	MySharedPtr* operator->() { return pData; };
+	MySharedPtr& operator*() { return *pData;};
+	MySharedPtr* operator->() { return pData ;};
 
 	// Assignment operator:
 	MySharedPtr<T>& operator=(MySharedPtr<T>& from) {
@@ -38,4 +38,10 @@ private:
 
 };
 
-#endif // _MY_SHARED_PTR_H_
+int main() {
+	MySharedPtr<int> p1(new int);
+	*p1 = 42;
+	cout << (*p1) << endl;
+	cout << (*(p1-get())) << endl;
+	MySharedPtr<int> p1(p2);
+};
