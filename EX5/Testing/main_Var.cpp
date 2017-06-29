cacdec3e5d01ed4212311f8c5cabdfcdb1c452a6
Gur Chemel <g.chemel@ef.technion.ac.il>
2017-06-29 12:14:21 +0300

    Finished matrix, compiling not succesfull..

---
+++ EX5/Testing/main_Var.cpp
@@ -0,0 +1,21 @@
+
+#include "MySharedPtr.h"
+#include "Variable.h"
+#include "Scalar.h"
+#include "Matrix.h"
+#include "ScriptExceptions.h"
+
+using namespace std;
+
+int main(){
+	
+	VarPtr	Test;
+	Scalar testing = 5;
+	//VarPtr	FirstS = new Scalar(5);
+	//VarPtr	SecondS = VarPtr(new Scalar(6));
+	//VarPtr	Third = FirstS + SecondS;
+
+//	cout << "Ouput: " << Third[1] << endl;
+
+	return 0;
+}
